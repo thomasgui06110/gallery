bb03f3b1646f23782d037f7042bf6c7fd4d176c5
thomasgui <guilhem.thomas@gmail.com>
2020-01-22 13:04:13 +0100

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: '.env'
+})
+
 module.exports = {
   siteMetadata: {
     title: `Gallery 122`,
@@ -45,9 +49,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "https://gallery.planethoster.world/gallery",
+        baseUrl: process.env.API_URL,
         // The protocol. This can be http or https.
-        protocol: "https",
+        protocol: process.env.API_PROTOCOL,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
@@ -144,6 +148,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    'gatsby-plugin-netlify'
   ],
 
 }
