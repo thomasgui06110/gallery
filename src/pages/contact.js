d02d0b5f933258a31dec2d0e834fbccc6c988e14
thomasgui <guilhem.thomas@gmail.com>
2020-01-25 18:52:55 +0100

    modification form3

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -32,6 +32,9 @@ const Contact = () => {
             <h3>Envoyez-nous un message</h3>
             <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <input type="hidden" name="form-name" value="Contact Form" />
+            <p>
+    <label>Your Name: <input type="text" name="name2" /></label>   
+  </p>
               <Form>
                 <Form.Group controlId="formBasicEmail">
                   <Form.Label>Your Name / Votre Nom</Form.Label>
