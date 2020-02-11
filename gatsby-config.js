c6382f4354b31ecf9b39b1a0a71520ee5cdd5eb1
thomasgui <guilhem.thomas@gmail.com>
2020-02-11 10:56:40 +0100

    suppr intl

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,19 +148,7 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: `gatsby-plugin-intl`,
-      options: {
-        // language JSON resource path
-        path: `${__dirname}/src/intl`,
-        // supported language
-        languages: [`en`, `fr`],
-        // language file path
-        defaultLanguage: `fr`,
-        // option to redirect to `/ko` when connecting `/`
-        redirect: true,
-      },
-    },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
